8dbe32d564316a2e24103735dbb9645d733c8a6d
Krystal Sanchez <krystal_sanchez3@my.vcccd.edu>
2021-03-23 06:48:56 +0000

    initialized number variables and prompts user to enter.

---
+++ quiz-q3.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main(){
+  int num1,num2,num3;
+  cout << "Enter three integer values, each number must be between 0 - 99: ";
+  cin >> num1 >> num2 >> num3 ;
+
