7916b92ad0625d2698472694564aaace1b975131
Krystal Sanchez <krystal_sanchez3@my.vcccd.edu>
2021-03-23 06:34:15 +0000

    created variables and prompts user to enter ranges

---
+++ quiz-q5.cpp
@@ -0,0 +1,12 @@
+  #include <iostream>
+#include <iomanip>
+using namespace std;
+int main() 
+{
+  int i,num;
+  int flag;
+  int range1,range2;
+  
+  cout << "Enter your range: ";
+  cin >> range1 >> range2;
+  
