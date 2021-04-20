d74b79e1b20678f8a3d741b75e999fcb28459f83
Krystal Sanchez <>
2021-04-20 05:53:44 +0000

    displaying random numbers

--- Midterm/Exam1q3.cpp
+++ Midterm/Exam1q3.cpp
@@ -5,9 +5,13 @@ using namespace std;
 
 int getRdnum(void);
 
+
 int main(){
   int randnum;
   srand(time(0));
-
-  cout<<
+  
+  cout<<"Num1 is: " << num3<< endl;
+  cout<<"Num2 is: " << num2<<endl;
+  cout<<"Num3 is: "
+  
 }
