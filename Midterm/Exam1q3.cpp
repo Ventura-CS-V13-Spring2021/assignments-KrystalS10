5b078eb03d025ce9fcfdf4851422924d12be8a1d
Krystal Sanchez <>
2021-04-20 05:54:15 +0000

    num3

--- Midterm/Exam1q3.cpp
+++ Midterm/Exam1q3.cpp
@@ -10,8 +10,8 @@ int main(){
   int randnum;
   srand(time(0));
   
-  cout<<"Num1 is: " << num3<< endl;
+  cout<<"Num1 is: " << num1<< endl;
   cout<<"Num2 is: " << num2<<endl;
-  cout<<"Num3 is: "
+  cout<<"Num3 is: " << num3<<endl;
   
 }
