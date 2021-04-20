36c5a371ecd8981eac3d0b017cd9b4dd9c10f330
Krystal Sanchez <>
2021-04-20 04:29:50 +0000

    created variables

--- Midterm/Exam1q2.cpp
+++ Midterm/Exam1q2.cpp
@@ -2,18 +2,16 @@
 #include <fstream>
 using namespace std;
 
+int getrandNum();
+int greaterCheck();
+
+const int N = 10;
+
+
 int main()
 {
+  int randNum;
+	ofstream ofs;
 
-	ofstream file;
-  file.open("number.text");
-
-  srand((unsigned)(10));
-  int ran_num;
-
-  for(int index = 0; index<50; index++)
-  {
-    ran_num = ()
-  }
 }
 	
