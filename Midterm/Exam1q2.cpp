3e0028a733c24f5111a84c6d6b60ed1b722b74e3
Krystal Sanchez <>
2021-04-20 04:21:50 +0000

    exam question 2

---
+++ Midterm/Exam1q2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+
+	ofstream file;
+  file.open("number.text");
+
+  srand((unsigned)(10));
+  int ran_num;
+
+  for(int index = 0; index<50; index++)
+  {
+    ran_num = ()
+  }
+}
+	
