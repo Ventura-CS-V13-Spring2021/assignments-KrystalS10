7d80af7223f04b4812aeac05bd90a2a4eb4eaa66
Krystal Sanchez <>
2021-04-20 06:52:06 +0000

    created findMax function

--- Midterm/Exam1q3.cpp
+++ Midterm/Exam1q3.cpp
@@ -5,19 +5,21 @@ using namespace std;
 
 int getRdnum(void);
 
+int num1 = getRdnum();
+int num2 = getRdnum();
+int num3 = getRdnum();
+
+int findMax(void);
+int maxnum = findMax();
 
 int main(){
-  //int randnum;
+  
   srand(time(0));
 
-  int num1 = getRdnum();
   cout<<"Num1 is: " << num1<< endl;
-  int num2 = getRdnum();
   cout<<"Num2 is: " << num2<<endl;
-  int num3 = getRdnum();
   cout << "Num3 is: " << num3<<endl;
-
-  
+  cout << maxnum << endl;
 }
 
 int getRdnum(){
@@ -25,4 +27,26 @@ int getRdnum(){
   rdnum = rand()% 9 + 1;
   return rdnum; 
 
+}
+
+int findMax(){  
+  if(num1 > num2 && num1 > num3)
+  {
+    int maxnum;
+    cout << num1 << " is the max amoung the three integers" ;
+    maxnum = num1;
+  }
+  else if (num2 > num1 && num2 > num3){
+     int maxnum;
+    cout << num2 << " is the max amoung the three integers" ;
+    maxnum = num2;
+  }
+  
+  else if(num3 > num1 && num3 > num2){
+     int maxnum;
+    cout << num3 << " is the max amoung the three integers" ;
+    maxnum = num3;
+  }
+    return maxnum;
+  
 }
