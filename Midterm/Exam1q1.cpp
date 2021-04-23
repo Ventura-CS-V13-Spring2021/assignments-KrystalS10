70972e0501896d517011ba6a40e8ea717827e296
Krystal Sanchez <>
2021-04-23 08:58:48 +0000

    fixed minor details

--- Midterm/Exam1q1.cpp
+++ Midterm/Exam1q1.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 int main()
 {
-  ifstream ffs;
+  ifstream ifs;
   string name;
   int score_1, score_2;
   int sum;
   float average;
   int total_num = 0;
   
-  ffs.open("students.txt");
+  ifs.open("students.txt");
 
-  if(!ffs){
+  if(!ifs){
     cerr << "File open error!\n";
     exit(0);
   }
 
-  while(ffs >> name >> score_1 >> score_2)
+  while(ifs >> name >> score_1 >> score_2)
   {
     cout<< "Student name: " << name << "\t";
     cout<< "Score 1: " << score_1 << "\t" << "Score 2: " << score_2 << "\t";   
@@ -32,12 +32,12 @@ int main()
     cout << "Avg: " << average << "\t";
 
     if (average > 80)
-      total_num = total_num + 1;
+      total_num++;
 
     cout << "\n"<< endl;
 
   }
 
-  cout << "\nThe total number of students with an average higher than 80 is: " << total_num << endl;
-  ffs.close();
+  cout << "\nThe total number of students with an average greater than 80 is: " << total_num << endl;
+  ifs.close();
 }
