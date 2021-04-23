8b7d41c0cb5e1cd81b7cd1189bd1effb37b959f8
Krystal Sanchez <>
2021-04-23 08:28:12 +0000

    created sum variable,made while loop display information while there was data in the file.

--- Midterm/Exam1q1.cpp
+++ Midterm/Exam1q1.cpp
@@ -9,11 +9,20 @@ int main()
   ifstream ffs;
   string name;
   int score_1, score_2;
+  int sum;
   
   ffs.open("students.txt");
 
   if(!ffs){
     cerr << "File open error!\n";
     exit(0);
+  }
+
+  while(ffs >> name >> score_1 >> score_2)
+  {
+    cout<< "Student name: " << name << "\t";
+    cout<< "Score 1: " << score_1 << "\t" << "Score 2: " << score_2 << "\t";   
+
+    sum = score_1 + score_2; 
   }
 }
