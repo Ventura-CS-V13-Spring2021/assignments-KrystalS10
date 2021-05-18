9a701cf0f5b11fabadd8068271cae1f62fda74cb
Krystal Sanchez <>
2021-05-18 19:59:48 +0000

    ...

--- finalQ2/finalQ2.cpp
+++ finalQ2/finalQ2.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
+using namespace std;
 
 void swap( int i, int j){
-  int temp;
+  int temp=0;
   temp = i;
   i = j;
   j = temp;
 
 }
-void getArrayCount(int arr[]){
+// void getArrayCount(int arr[]){
+  
+// }
+
+int main() {
+  int arr[] = { -12, 3, -12, 4, 1, 1, -12, 1 , -1, 1, 2, 3, 4, 2, 3 -12};
   int len = sizeof(arr)/ sizeof(arr[0]);
+  int sortedArr[len];
+  
   for(int i= 0; i < len; i++){
-    for(int j = i+1; j< len; j++){
+    for(int j = 0; j< len; j++){
       if(arr[i] < arr[j]){
         swap(arr[i], arr[j]);
       }
       
+      
     }
   }
-}
+  int count;
+  for(int i = 0; i<= len; i++){
+    count = 0;
+    for(int j = 0; j<= len; j++){
+      if(arr[i] == arr[j]){
+        count++;
+      }
+    }
+    cout<<arr[i] << "  "<< count<< endl;
+  }
 
-int main() {
-  int arr[] = { -12, 3, -12, 4, 1, 1, -12, 1 , -1, 1, 2, 3, 4,2,3 -12};
-  
+  // for(int i: arr){
+  //   cout<< arr[i]<< " ";
+  // }
 
 }
