92ce20fa95dedc8a700a6dec7f9dc043c1f19174
Krystal Sanchez <>
2021-05-18 19:28:46 +0000

    took out function getNumDiv, didnt use/need

--- finalQ1/finalQ1.cpp
+++ finalQ1/finalQ1.cpp
@@ -1,30 +1,33 @@
 #include <iostream>
+#include <array>
 using namespace std;
 
-int getNumDiv(int num){
-  int count = 0;
-    for(int i =0; i < num; i++ ){
-      if(num % i == 0){
+int main() {
+  int arr[] = {1, 2, 4, 6, 10, 24};
+  int len = sizeof(arr)/sizeof(arr[0]);
+  int divElem[len];
+
+  int lenDiv = sizeof(divElem)/sizeof(divElem[0]);
+
+  for(int i = 0; i < len; i++){
+    int count = 0;
+    for(int j = 1; j < len; j++){
+      if(arr[i] % arr[j]==0){
         count++;
       }
     }
-    return count;
-}
-int main() {
-  int arr[] = {1, 2, 4, 6, 10, 24};
-  int divElem[arr.length()];
-  for(int i = 0; i < arr.length; i++){
-    divElem[i] = getNumDiv(arr[i]);
+    divElem[i] = count;
+    
   }
   int max = 0;
-  for(int i = 0; i< divElem.length(); i++){
+  int elem = 0;
+  for(int i = 0; i< lenDiv; i++){
    
       if(divElem[i]> max){
         max = divElem[i];
-
+        elem = i;
       } 
   }
-
+  cout<<"Element " << arr[elem] << " has the greatest number of divisible elements which is " << max;
   
 }
-
