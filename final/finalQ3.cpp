e675d7ba0856546388ddd89f19779cb1eca85386
Krystal Sanchez <>
2021-05-18 21:55:31 +0000

    finished main function, getName function and getNumClasses.

--- final/finalQ3.cpp
+++ final/finalQ3.cpp
@@ -34,6 +34,7 @@ Student::Student(string n, int num){
        cin >> classList[i];
    }
 }
+
 // Student Student::&operator= (Student &rhs){
 
 // }
@@ -41,7 +42,46 @@ Student::Student(string n, int num){
 Student::~Student(){
   delete[] classList;
 }
+
 void Student::resetClass(){
   delete[] classList;
   this->numClasses =0;
 }
+void Student::inputClass(string n, int num){
+   this->name = n;
+  this-> numClasses = num;
+  this-> classList = new string[num];
+   for (int i = 0; i < numClasses; i++)
+   {
+       cout << "Enter the course name : ";
+       cin >> classList[i];
+   }
+}
+
+void Student::printAll()const{
+  cout<< "Name: " << this->name <<endl;
+  cout<< "Classes: "<< endl;
+  for(int i = 0; i<numClasses; i++ ){
+    cout << classList[i] << endl;
+  }
+}
+
+string Student::getName()const{
+  return this->name;
+}
+
+int Student::getNumClasses()const{
+  return this->numClasses;
+}
+
+int main(){
+  Student s("Krystal", 0);
+  s.inputClass("Krystal", 3);
+  s.printAll();
+  cout<<s.getNumClasses()<<endl;
+  cout<<s.getName()<< endl;
+  s.resetClass();
+  cout<<s.getNumClasses()<<endl;
+}
+
+#endif 
